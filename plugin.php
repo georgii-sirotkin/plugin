b1dd118d252a3861f649f61981b7c525f0e2e7c1
Georgii Sirotkin <georgiysirotkin@gmail.com>
2020-12-13 23:07:58 -0500

    Display price in USD

--- plugin.php
+++ plugin.php
@@ -3,3 +3,22 @@
  * Plugin Name: Final project
  */
 
+function getExchangeRates() {
+    $url = "https://api.exchangeratesapi.io/latest?base=CAD";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    return json_decode($result, true)['rates'];
+}
+
+function displayPriceInUSD() {
+    $price = auto_listings_meta('price');
+    $exchangeRates = getExchangeRates();
+    $cadToUsd = $exchangeRates['USD'];
+    echo '<div style="text-align: right; margin-top: -20px; font-size: 17px;">';
+    echo '<strong>Price in USD: $' . number_format(round($price * $cadToUsd)) . '</strong>';
+    echo '</div>';
+}
+
+add_action('auto_listings_single_content', 'displayPriceInUSD');
